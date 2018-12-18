743e1050ceac8998b6c26e8583b4df45a5f47015
newpanjing <newpanjing@163.com>
2018-12-18 10:49:39 +0800

    增加过滤器和自定义方法

--- config.js
+++ config.js
@@ -83,127 +83,37 @@ exports.config = {
         dates: {
             package: 'java.util.Date',
             typeName: 'Date',
-            types: ["DATE", "TIME", "DATETIME", "TIMESTAMP", "YEAR"]
+            types: ["DATE", "DATETIME", "TIMESTAMP", "YEAR"]
+        },
+        times: {
+            package: 'java.sql.Time',
+            typeName: 'Time',
+            types: ["TIME"]
         }
 
     },
-    //模块
-    models: {
-        AdSpace: {
-            table: 'ad_space',
-            remark: '广告位模块',
-            url: '/ad/space/',
-            methods: [{
-                name: 'saveTest',
-                remark: '保存测试',
-                url: '/test',
-                //result 分为map和list，如果是list直接add 字段，如果是map xxx.put(key,value)
-                //一个 字段的类型，多个object
-                //单个字段 单个返回
-                result: ['$this'],
-                // result: {$ref:['id','createTime']},
-                actions: [{
-                    'save': {
-                        id: '$uuid',
-                        createTime: '$now',
-                        updateTime: '$createTime',
-                        alias: 'aaa',
-                        type: 1
-                    }
-                }]
-            }, {
-                name: 'updateTest',
-                remark: '更新测试',
-                url: '/update',
-                result: '$this',
-                actions: [{
-                    "update": {
-                        $ref: {
-                            id: '$id',
-                            name: '123',
-                            type: 111,
-                        },
-                        updateTime: '$now',
-                        createTime: '$createTime'
-                    }
-                }]
-            }]
-        },
-        AdSpaceItem: {
-            table: 'ad_space_item',
-            remark: '广告位详情',
-            url: '/ad/space/item/',
-        },
-        Contact: {
-            table: 'contact',
-            remark: '联系人',
-            url: '/contact',
-            verify: {
-                save: ['userId', 'mobile', 'name'],
-                update: ['id'],
-                delete: ['id'],
-                list: ['userId']
-            }
-        },
-        Order: {
-            table: 'order',
-            remark: '订单',
-            url: '/order'
-        },
-        OrderItem: {
-            table: 'order_item',
-            remark: '订单详情',
-            url: '/order/item',
-        },
-        OrderComment: {
-            table: 'order_comment',
-            remark: '订单评价',
-            url: '/order/comment',
-            verify: {
-                save: ['userId', 'orderId', 'star', 'content'],
-                update: ['id'],
-                delete: ['id'],
-                list: ['userId']
-            }
-        },
-        OrderRefund: {
-            table: 'order_refund',
-            remark: '订单退款',
-            url: '/order/refund'
-        },
-        Scenic: {
-            table: 'scenic',
-            remark: '景点',
-            url: '/scenic'
-        },
-        ScenicTag: {
-            table: 'scenic_tag',
-            remark: '景点标签',
-            url: '/scenic/tag'
-        },
-        ScenicTagRelation: {
-            table: 'scenic_tag_relation',
-            remark: '景点标签映射'
-        },
-        ScenicTicket: {
-            table: 'scenic_ticket',
-            remark: '景点门票'
-        }
-    },
     //过滤器，执行每个model的时候调用
-    filter: function (model,template) {
-        // if (template.template == 'controller.fc') {
-        //     //不生成控制器
-        //     return false;
-        // }
+    filter: function (model, template) {
+
+        if (template.template != 'mapper.xml.fc') {
+            return false;
+        }
         return true;
     },
     //扩展方法，执行完所有模块后调用
     extends: [function () {
 
         //生成api文档
-        require('./builder-doc').build(this.models,()=>{
+        require('./builder-doc').build(this.models, () => {
             console.log('文档生成完成。');
         });
-    }]
+    }],
+    //模块
+    models: {
+        ScenicTag: {
+            table: 'scenic_tag',
+            remark: '景区标签',
+            url: 'scenic/tag'
+        }
+    }
 }
